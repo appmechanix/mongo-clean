c715cd349177f7e3c85cd554892f351a2f874c65
Daniel Wylie <daniel@danielwylie.me>
2014-12-17 23:28:49 +1300

    Ability to add keys to strip

--- README.md
+++ README.md
@@ -11,15 +11,40 @@ Installing
 npm install dwnz-mongo-clean --save
 ```
 
+Setup
+=====
+```js
+var cleaner = require('dwnz-mongo-clean');
+```
+
+Settings
+========
+
+You can supply keys that you want to ignore on an application wide context.
+
+```js
+cleaner.addGlobalKey('KeyToIgnore');
+```
+
 Using
 =====
 ```js
-var clean = require('mongo-clean').clean;
-
 var item = {
 	Name: "Mongo",
 	AccountKey: "123"
 };
 
-console.log(clean(item));
+var cleanItem = cleaner.clean(item);
+```
+
+You can also specify keys to ignore in specific cleaning cases
+
+```js
+var item = {
+	Name: "Mongo",
+	AccountKey: "123",
+	Test: "Test"
+};
+
+var cleanItem = cleaner.clean(item, ['Test']);
 ```
--- test/tests.js
+++ test/tests.js
@@ -1,53 +1,159 @@
 var clean = require('../lib/index').clean;
 
-exports.TestThatAccountKeyIsStripped = function(test){
-	var item = {
-		Name: "Mongo",
-		AccountKey: "123"
-	};
+exports.TestThatAccountKeyIsStripped = function (test) {
+    var item = {
+        Name: "Mongo",
+        AccountKey: "123"
+    };
 
-	item = clean(item);
-    
+    item = clean(item);
+
     test.equal(undefined, item.AccountKey);
     test.done();
 };
 
-exports.TestThat__vIsStripped = function(test){
-	var item = {
-		Name: "Mongo",
-		__v: 123
-	};
+exports.TestThat__vIsStripped = function (test) {
+    var item = {
+        Name: "Mongo",
+        __v: 123
+    };
 
-	item = clean(item);
-    
+    item = clean(item);
+
     test.equal(undefined, item.AccountKey);
     test.done();
 };
 
-exports.TestThatUserIdIsStripped = function(test){
-	var item = {
-		Name: "Mongo",
-		UserId: 123
-	};
+exports.TestThatUserIdIsStripped = function (test) {
+    var item = {
+        Name: "Mongo",
+        UserId: 123
+    };
 
-	item = clean(item);
-    
+    item = clean(item);
+
     test.equal(undefined, item.AccountKey);
     test.done();
 };
 
-exports.TestThatEverythingIsStripped = function(test){
-	var item = {
-		Name: "Mongo",
-		AccountKey: "123",
-		__v: 123,
-		UserId: 123
-	};
+exports.TestThatEverythingIsStripped = function (test) {
+    var item = {
+        Name: "Mongo",
+        AccountKey: "123",
+        __v: 123,
+        UserId: 123
+    };
 
-	item = clean(item);
-    
+    item = clean(item);
+
     test.equal(undefined, item.AccountKey);
     test.equal(undefined, item.__v);
     test.equal(undefined, item.UserId);
+    test.done();
+};
+
+exports.TestThatItemsInArraysAreStripped = function (test) {
+    var items = [
+        {
+            Name: "Mongo",
+            AccountKey: "123"
+        },
+        {
+            Name: "Mongo",
+            AccountKey: "123"
+        },
+        {
+            Name: "Mongo",
+            AccountKey: "123"
+        },
+        {
+            Name: "Mongo",
+            AccountKey: "123"
+        }
+    ];
+
+    items = clean(items);
+
+    for (var i = 0; i < items.length; i++) {
+        test.equal(undefined, items[i].AccountKey);
+    }
+
+    test.done();
+};
+
+exports.TestThatCustomKeyIsRemovedFromSingleEntity = function (test) {
+    var item = {
+        Name: "Mongo",
+        AccountKey: "123",
+        Password: "Test"
+    };
+
+    item = clean(item, ['Password']);
+
+    test.equal(undefined, item.AccountKey);
+    test.equal(undefined, item.Password);
+
+    test.done();
+};
+
+exports.TestThatCustomKeyIsRemovedFromArray = function (test) {
+    var items = [
+        {
+            Name: "Mongo",
+            AccountKey: "123",
+            Password: "Test"
+        },
+        {
+            Name: "Mongo",
+            AccountKey: "123",
+            Password: "Test"
+        },
+        {
+            Name: "Mongo",
+            AccountKey: "123",
+            Password: "Test"
+        },
+        {
+            Name: "Mongo",
+            AccountKey: "123",
+            Password: "Test"
+        }
+    ];
+
+    items = clean(items);
+
+    for (var i = 0; i < items.length; i++) {
+        test.equal(undefined, items[i].AccountKey);
+        test.equal(undefined, items[i].Password);
+    }
+
+    test.done();
+};
+
+exports.TestThatExtraKeyNonArrayThrowsAnError = function (test) {
+    var item = {
+        Name: "Mongo",
+        AccountKey: "123"
+    };
+
+    test.throws(function () {
+        clean(item, 'test')
+    }, Error, "removeKeys must be an array");
+    test.done();
+};
+
+exports.TestThatAddingAGlobalRemoveKeyRemovesItFromTheList = function (test) {
+    require('../lib/index').settings.globalRemove.push('Test');
+
+    var item = {
+        Name: "Mongo",
+        AccountKey: "123",
+        Test: "Test"
+    };
+
+    item = clean(item);
+
+    test.equal(undefined, item.Test);
+
     test.done();
 };
